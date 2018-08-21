e92801e44abf9052ffe607c5932e103b054d0ad2
Xueying <xueying@xueyings-mbp-2.dhcp.nd.edu>
2018-08-21 11:56:20 -0400

    01

--- easy01.py
+++ easy01.py
@@ -1,7 +1,14 @@
 
-def haha():
-	print haha
+def twoSum(nums, target):
+        s = []      
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[j] == target - nums[i]:
+                    s.append(i)
+                    s.append(j)
+                    return s
 
 if __name__ == '__main__':
 
-	haha()
+	print twoSum([2, 7, 11, 15], 9)
+	
