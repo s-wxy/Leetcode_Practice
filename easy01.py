cedd4180f3dc05539cf26bce59041ce490252407
Xueying <xueying@xueyings-mbp-2.dhcp.nd.edu>
2018-08-21 13:57:22 -0400

    easy02

--- easy01.py
+++ easy01.py
@@ -8,7 +8,35 @@ def twoSum(nums, target):
                     s.append(j)
                     return s
 
+
+def reverse(x):	
+	if x > 0:	
+		x = str(x)	
+		if x[-1] == '0':
+			x = x[:-1]
+			x = x[::-1]
+		else:
+			x = x[::-1]
+	elif x < 0:
+		x = str(x)
+		if x[-1] == '0':
+			x = x[1:]
+			x = x[:-1]
+			x = x[::-1]
+			x = '-' + x
+		else:
+			x = x[1:]
+			x = x[::-1]
+			x = '-' + x
+	x = int(x)
+	if x > -2**31 and x < ((2**31) -1):
+		return x
+	else:
+		return 0 
+
+
 if __name__ == '__main__':
 
-	print twoSum([2, 7, 11, 15], 9)
+	#print twoSum([2, 7, 11, 15], 9)
+	print reverse(1534236469)
 	
