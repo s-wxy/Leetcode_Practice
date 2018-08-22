377a8e0c60ada4acadadb8610edec8f142582178
Xueying <xueying@xueyings-mbp-2.dhcp.nd.edu>
2018-08-22 15:54:55 -0400

    easy05

--- easy01.py
+++ easy01.py
@@ -12,21 +12,15 @@ def reverse(x):
 	if x > 0:	
 		x = str(x)	
 		if x[-1] == '0':
-			x = x[:-1]
-			x = x[::-1]
+			x = x[:-1][::-1]
 		else:
 			x = x[::-1]
 	elif x < 0:
 		x = str(x)
 		if x[-1] == '0':
-			x = x[1:]
-			x = x[:-1]
-			x = x[::-1]
-			x = '-' + x
+			x = '-' + x[1:][:-1][::-1]
 		else:
-			x = x[1:]
-			x = x[::-1]
-			x = '-' + x
+			x = '-' +  x[1:][::-1]
 	x = int(x)
 	if x > -2**31 and x < ((2**31) -1):
 		return x
@@ -42,10 +36,22 @@ def strStr(haystack, needle):
 	else:
 		return -1
 
+def longestCommonPrefix(strs):
+	if len(strs) == 0:
+		return ""
+	else:
+		s1 = min(strs)
+		s2 = max(strs)
+		for i, c in enumerate(s1):
+			if c != s2[i]:
+				return s1[:i]
+		return s1
+
 
 if __name__ == '__main__':
 
 	#print twoSum([2, 7, 11, 15], 9)
 	#print reverse(1534236469)
-	print strStr("hello", "ll")
+	#print strStr("hello", "ll")
+	print longestCommonPrefix(["dog","racecar","car"])
 	
