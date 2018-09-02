a695e89ff3e76e599c2a65c69f7f23285498115c
Xueying <xueyingwang.x@gmail.com>
2018-09-02 12:18:27 +0800

    23

--- easy03.py
+++ easy03.py
@@ -19,7 +19,45 @@ def peakIndexInMountainArray(A):
 
 # 22, Array Partition I
 def arrayPairSum(nums):
-	
+	return sum(sorted(nums)[::2])
+	# list[start:end:step]
+
+# 23, self dividing numbers 
+def selfDividingNumbers(left,right):
+	out = []
+	for num in range(left,right+1):
+		s_num = str(num)
+		l_num = len(s_num)
+		count = 0
+		if "0" in s_num:
+			continue
+		else:
+			for d in s_num:
+				if num % int(d) == 0:
+					count += 1 
+			if count == l_num:
+				out.append(num)
+	return out
+
+	# not invert to str
+	# result = []
+	# for i in range(left, right+1):
+	# 	if i < 10:
+	# 		result.append(i)
+	# 	else:
+	# 		flag = True
+	# 		num = i
+	# 		while num > 0:
+	# 			rem = num % 10
+	# 			if not rem or i % rem:
+	# 				flag = False
+	# 				break
+	# 			num = num / 10
+	# 		if flag:
+	# 			result.append(i)
+	# return result
 
 
 if __name__ == '__main__':
+
+	print selfDividingNumbers(1,22)
