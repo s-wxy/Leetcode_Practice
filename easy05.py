d53f8149af737067be5989ba6be504c59bab03c4
Xueying <xueyingwang.x@gmail.com>
2018-09-06 17:46:51 +0800

    41

--- easy05.py
+++ easy05.py
@@ -1,7 +1,18 @@
-#41, 
+from collections import Counter
 
+#41, Single Number
+def singleNumber(nums):
+    dic = {}
+    for num in nums:
+        dic[num] = dic.get(num, 0)+1
+    for key, val in dic.items():
+        if val == 1:
+            return key
+	
 
 
 
 
 if __name__ == '__main__':
+
+	print singleNumber([2,2,1])
