fbc626c0e318cc6d4cb48e9b3fab7c52aa7f8472
Xueying <xueyingwang.x@gmail.com>
2018-10-20 10:48:18 +0800

    ll

--- linklist.py
+++ linklist.py
@@ -1,6 +1,7 @@
 
 '''
 # 1, Middle of the Linked List 
+# 2, Reverse Linked List - iterative, recursive
 
 '''
 
@@ -17,6 +18,7 @@ def middleNode(head):
 	# the other is tmp, with two steps each iteration 
 	# when tmp reach the end, head just reaches the half of it 
 
+def middleNode01(head):
 	if head == None:
 		return head
 	if head.next == None:
@@ -30,5 +32,15 @@ def middleNode(head):
 			q = q.next
 	return p
 
+# 2, Reverse Linked List
+def reverseList(self, head):
+	prev,cur = None, head
+	while cur: 
+		prev,cur,cur.next = cur.nect, prev, cur
+	return prev 
 
-if __name__ == '__main__':
+def reverseList01(self, head):
+	prev = None
+	if not head: return prev 
+	cur, head.next = head.next, prev 
+	return self.reverseList(curr, head)
